904aa5c82b5044c350383a7b73045986a9f9451e
Dip Dutt <emdutt456@gmail.com>
2025-08-12 17:12:10 +0600

    Create EditView with lable content ,modifier and stacks
    in this view we are updating data .

--- SwiftDataCrud/SwiftDataCrud/Views/EditView.swift
+++ SwiftDataCrud/SwiftDataCrud/Views/EditView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct EditView: View {
-     // MARK: - Properties
+    // MARK: - Properties
     @State private var status = Status.onShelf
     @State private var title: String = ""
     @State private var author: String = ""
@@ -17,8 +17,86 @@ struct EditView: View {
     @State private var dateAdded: Date = Date.distantPast
     @State private var dateStarted: Date = Date.distantPast
     @State private var dateCompleted: Date = Date.distantPast
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text("Status Type")
+            Picker("Show Status", selection: $status) {
+                ForEach(Status.allCases) { status in
+                    Text(status.description).tag(status)
+                }
+            }
+            .buttonStyle(.bordered)
+        }
+        VStack(alignment: .leading) {
+            GroupBox {
+                LabeledContent {
+                    DatePicker("", selection: $dateAdded, displayedComponents: .date)
+                } label: {
+                    Text("Date Added")
+                }
+                
+                if status == .inProgress || status == .completed {
+                    LabeledContent {
+                        DatePicker("", selection: $dateStarted, displayedComponents: .date)
+                    } label: {
+                        Text("Date Started")
+                    }
+                }
+                if status == .completed {
+                    LabeledContent {
+                        DatePicker("", selection: $dateCompleted, displayedComponents: .date)
+                    } label: {
+                        Text("Date Completed")
+                    }
+                }
+            }
+            .onChange(of: status){ oldValue, newValue in
+                if newValue == .onShelf {
+                    dateAdded = Date.distantPast // book end
+                    dateStarted = Date.distantPast
+                    
+                }
+                else if newValue == .inProgress && oldValue == .completed {
+                    dateStarted = Date.distantPast // book started to end
+                }
+                
+                else if newValue == .inProgress && oldValue == .onShelf {
+                    dateStarted = Date.now // book started
+                }
+                
+                else if newValue == .completed  && oldValue == .onShelf{
+                    dateCompleted = Date.now // forget the book
+                }
+                else {
+                    dateCompleted = Date.now
+                }
+            }
+            .foregroundStyle(.secondary)
+            
+            Divider()
+            
+            LabeledContent {
+              TextField("enter bookTitle", text: $title)
+            } label: {
+                Text("BookTitle")
+            }
+            
+            LabeledContent {
+              TextField("enter author name", text: $title)
+            } label: {
+                Text("Author Name")
+            }
+            Divider()
+            Text("Summary")
+            TextEditor(text: $summary)
+                .padding(5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.gray, lineWidth: 1)
+                    )
+        }
+        .padding()
     }
 }
 
