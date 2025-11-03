0cc9bb1cdcd784b08eb86c8a32dc7f3fc9b314fe
Dip Dutt <emdutt456@gmail.com>
2025-11-03 11:53:06 +0600

    add mark

--- SwiftDataCrud/SwiftDataCrud/Views/QuoteListView.swift
+++ SwiftDataCrud/SwiftDataCrud/Views/QuoteListView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct QuoteListView: View {
+    // MARK: - Properties
     @Environment(\.managedObjectContext) var context
     let book:Book
+   // MARK: - Body
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+ // MARK: - Preview
 #Preview {
     let preview = Preview()
     preview.addExamples(Book.sampleBooks)
